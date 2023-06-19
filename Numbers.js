05ea73229c0a3fe0ec5f1679e680b5b9c22ad57e
fer.belen <belenxmorales@gmail.com>
2023-06-19 20:58:28 -0300

    Numbers

---
+++ Numbers.js
@@ -0,0 +1,26 @@
+// Una variable que contenga tu altura en centímetros (entero)
+let altura = 165;
+console.log(altura);
+
+//Una variable que contenga tu altura en metros (número de coma flotante)
+let altura1 = 1.65;
+console.log(parseFloat(altura1))
+
+//Una variable que contenga tu peso en Kilogramos(número de coma flotante)
+let peso = 70.00;
+console.log(parseFloat(peso));
+
+//Una variable que contenga tu altura en metros redondeada hacia arriba
+
+let altura3 = 1.65;
+console.log(Math.ceil(altura3))
+
+//Una variable que contenga tu peso en kilogramos redondeado hacia abajo
+let altura4 = 1.65;
+console.log(Math.floor(altura4));
+
+//Una variable que contenga si "el máximo valor que se puede obtener en Javascript + 1" es igual al máximo valor que se puede obtener en Javascript
+
+let max_valor = Number.MAX_VALUE;
+let num5 = 1;
+console.log(max_valor + num5)
